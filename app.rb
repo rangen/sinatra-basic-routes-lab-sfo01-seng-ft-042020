8ae7b8883aa437e237351e86d0ab521e1bcad671
Don Mallory <dmm333@gmail.com>
2020-05-11 15:06:23 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Don"
+    end
+
+    get '/hometown' do
+        "My hometown is Oakland"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is All Directions"
+    end
+
+
 end
